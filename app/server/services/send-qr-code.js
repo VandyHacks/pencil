eec5c37fc195af970df0b9951f1c05764e6ea51b
cktang88 <arctangent20@yahoo.com>
2018-06-24 17:40:50 -0400

    Update all dates

--- app/server/services/send-qr-code.js
+++ app/server/services/send-qr-code.js
@@ -6,14 +6,14 @@ const getQr = data => qr.imageSync(data, { type: 'png', size: 10, margin: 4 });
 const baseData = () => {
   return {
     from: 'VandyHacks <info@vandyhacks.org>',
-    subject: 'VandyHacks IV - Important information regarding check-in'
+    subject: 'VandyHacks V - Important information regarding check-in'
   };
 };
 
 module.exports = (email, id) => {
   mailgun.messages().send(Object.assign(baseData(), {
     to: email,
-    text: `We're excited to have you at VandyHacks IV! Please download the attached QR code to your phone before you arrive this evening; it'll be required to check into the event, any workshops, and any meals throughout the weekend. If you don't have it downloaded prior to check-in, you may be asked to go through the line again, so please come prepared!
+    text: `We're excited to have you at VandyHacks V! Please download the attached QR code to your phone before you arrive this evening; it'll be required to check into the event, any workshops, and any meals throughout the weekend. If you don't have it downloaded prior to check-in, you may be asked to go through the line again, so please come prepared!
 
 See you soon!
 
