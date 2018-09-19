7174b01ef894fbf895dc46f7efbb43c670c9a1be
williamjzhao <wzhao2017@gmail.com>
2018-09-18 23:47:53 -0500

    better whitelist

--- app/server/routes/cors.js
+++ app/server/routes/cors.js
@@ -1,6 +1,6 @@
 const corsOptions = {
   origin: function (origin, callback) {
-    if (origin === undefined || origin.endsWith('.vandyhacks.org')) {
+    if (origin === undefined || origin.endsWith('.vandyhacks.org') || origin.endsWith('connect-backend--vhf2018-qr-scanner.netlify.com')) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
