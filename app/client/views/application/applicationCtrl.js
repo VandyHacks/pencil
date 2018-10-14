35ab5dc82a5f9d62bb9e3c3835ec501e5be62546
cktang88 <arctangent20@yahoo.com>
2018-10-14 13:53:44 -0500

    Check empty essay length

--- app/client/views/application/applicationCtrl.js
+++ app/client/views/application/applicationCtrl.js
@@ -245,6 +245,10 @@ angular.module('app')
                   type: 'minLength',
                   value: 20,
                   prompt: 'Please answer the essay question.'
+                },
+                {
+                  type: 'empty',
+                  prompt: 'Please answer the essay question.'
                 }
               ]
             },
